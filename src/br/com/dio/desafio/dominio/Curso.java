16176cf04d2cda9a103ed8f63470194399adb5df
Tennison Capra <tennison_capra@hotmail.com>
2022-01-10 13:31:49 -0300

    minor changes

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -2,65 +2,13 @@ package br.com.dio.desafio.dominio;
 
 import java.util.Objects;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
+public class Curso extends Conteudo{
     private float cargaHoraria;
 
-    public void calcularXp(){
-        
-    }
 
-    public Curso(String titulo, String descricao, float cargaHoraria) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.cargaHoraria = cargaHoraria;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public float getCargaHoraria() {
-        return cargaHoraria;
-    }
-
-    public void setCargaHoraria(float cargaHoraria) {
-        this.cargaHoraria = cargaHoraria;
-    }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Curso curso = (Curso) o;
-        return Float.compare(curso.cargaHoraria, cargaHoraria) == 0 && titulo.equals(curso.titulo) && descricao.equals(curso.descricao);
+    protected double calcularXP() {
+        return 0;
     }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(titulo, descricao, cargaHoraria);
-    }
-
-    @Override
-    public String toString() {
-        return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", cargaHoraria=" + cargaHoraria +
-                '}';
-    }
-
 }
--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -3,35 +3,16 @@ package br.com.dio.desafio.dominio;
 import java.time.LocalDate;
 import java.util.Objects;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo{
     private LocalDate date;
 
-    public void calcularXp(){
-
+    @Override
+    protected double calcularXP() {
+        return 0;
     }
 
-    public Mentoria(String titulo, String descricao, LocalDate date) {
-        this.titulo = titulo;
-        this.descricao = descricao;
-        this.date = date;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    public Mentoria() {
+        this.date = LocalDate.now();
     }
 
     public LocalDate getDate() {
@@ -42,25 +23,6 @@ public class Mentoria {
         this.date = date;
     }
 
-    @Override
-    public String toString() {
-        return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", date=" + date +
-                '}';
-    }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Mentoria mentoria = (Mentoria) o;
-        return titulo.equals(mentoria.titulo) && descricao.equals(mentoria.descricao) && date.equals(mentoria.date);
-    }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(titulo, descricao, date);
-    }
 }
