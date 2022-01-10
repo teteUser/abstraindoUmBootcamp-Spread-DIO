eaaf6b38d12d780d7f3223d320cd7948033a3bb7
Tennison Capra <tennison_capra@hotmail.com>
2022-01-10 12:52:45 -0300

    minor changes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso("JAVA", "Curso básico de Java", 40f);
-
         System.out.println(curso1);
 
+        Mentoria mentoria1 = new Mentoria("Mentoria Java POO", "Entendendo POO", LocalDate.now());
+        System.out.println(mentoria1);
     }
 }
