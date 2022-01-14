94c6a8043283fcd8e4de9eb598631a222e69f6cf
Tennison Capra <tennison_capra@hotmail.com>
2022-01-14 11:49:15 -0300

    minor changes

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -3,19 +3,10 @@ package br.com.dio.desafio.dominio;
 import java.util.Objects;
 
 public class Curso extends Conteudo{
-    private int cargaHoraria;
 
     @Override
     protected double calcularXP() {
-        return XP_PADRAO * cargaHoraria;
-    }
-
-    public int getCargaHoraria() {
-        return cargaHoraria;
-    }
-
-    public void setCargaHoraria(Integer cargaHoraria) {
-        this.cargaHoraria = cargaHoraria;
+        return XP_PADRAO * (this.getCargaHoraria());
     }
 
     @Override
